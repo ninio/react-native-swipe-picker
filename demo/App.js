6ae6f79cfd3df817bb688f1f0906c51a76ed78bc
Nikolay Ninarski <n.ninarski@up2.nl>
2019-07-12 11:13:50 +0300

    Added the SwipePicker to the demo app.

--- demo/App.js
+++ demo/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SwipePicker from 'react-native-swipe-picker';
 
 export default function App() {
+  const items = [];
+
+  for (let index = 0; index < 100; index++) {
+    items.push({
+      value: index,
+      label: `Item ${index}`
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to see how to use SwipePicker!</Text>
+      <SwipePicker
+        items={ items }
+        onChange={ console.log }
+        height={ 80 }
+        width={ 120 }
+        />
     </View>
   );
 }
