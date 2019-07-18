b26b200395164cf3460d9d476a012abb0549c81d
Nikolay Ninarski <n.ninarski@up2.nl>
2019-07-19 00:33:09 +0300

    Enhanced the demo app with displaying of the onChange arguments

--- demo/App.js
+++ demo/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SwipePicker from 'react-native-swipe-picker';
 
 export default function App() {
   const items = [];
+
+  const [ selectedItem, setSelectedItem ] = useState({
+    index: 0,
+    item: {
+      value: 0,
+      label: 'Item 0'
+    }
+  } );
+
 
   for (let index = 0; index < 100; index++) {
     items.push({
@@ -17,10 +26,14 @@ export default function App() {
       <Text>Open up App.js to see how to use SwipePicker!</Text>
       <SwipePicker
         items={ items }
-        onChange={ console.log }
+        onChange={ setSelectedItem }
         height={ 80 }
         width={ 120 }
         />
+      <View style={ styles.selectedData }>
+        <Text>Selected Data:</Text>
+        <Text>{ JSON.stringify( selectedItem, null, '    ' ) }</Text>
+      </View>
     </View>
   );
 }
@@ -31,5 +44,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  selectedData: {
+    margin: 24,
+    padding: 12,
+    backgroundColor: 'rgba(200,200,0,0.15)',
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.75)',
   },
 });
